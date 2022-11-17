24c930074e66a3122535536c79852edfd811b4c2
André Lopes <75704128+andre-lopss@users.noreply.github.com>
2022-11-17 12:00:54 -0300

    updates

--- Secao6/Course/Course/Program.cs
+++ Secao6/Course/Course/Program.cs
@@ -4,14 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Point p;
+            //Point p;
 
-            p.X = 10;
-            p.Y = 20;
-            Console.WriteLine(p);
+            //p.X = 10;
+            //p.Y = 20;
+            //Console.WriteLine(p);
 
-            p = new Point();
-            Console.WriteLine(p);
+            //p = new Point();
+            //Console.WriteLine(p);
+
+            //Nullable
+            double? x = null;
+            double? y = 10.0;
+
+            double a = x ?? 5;
+            double b = y ?? 5;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
         }
     }
 }
