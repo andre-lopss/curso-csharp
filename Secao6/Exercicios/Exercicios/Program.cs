ee04840e7a510b0626ac917f6459a01083ab47be
André Lopes <andrelneres@hotmail.com>
2022-11-18 15:03:52 -0300

    Add Content and Practices

---
+++ Secao6/Exercicios/Exercicios/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many rooms will be rented? ");
+            int room = int.Parse(Console.ReadLine());
+            
+            Student[] vect = new Student[10];
+
+            for(int i = 1; i <= room; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"RENT #{i}:");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int chosenRoom = int.Parse(Console.ReadLine());
+                vect[chosenRoom] = new Student(name, email);    
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                    Console.WriteLine(i + ": " + vect[i]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos vazios: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] == null)
+                    Console.Write(i + " - ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Secao6/Exercicios/Exercicios/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercicios
+{
+    class Student
+    {
+        public string StudentName { get; set; }
+        public string StudentEmail { get; set; }
+
+        public Student(string nome, string email)
+        {
+            StudentName = nome;
+            StudentEmail = email;
+        }
+        public override string ToString()
+        {
+            return StudentName + ", " + StudentEmail;
+        }
+    }
+}
