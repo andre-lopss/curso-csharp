c55882f8ddcf6054ba9177e3c49f1267f8a2bbcb
André Lopes <andrelneres@hotmail.com>
2022-11-28 12:08:02 -0300

    updates

--- Secao10/Exercises/Exercise02/Exercise02/Entities/UsedProduct.cs
+++ Secao10/Exercises/Exercise02/Exercise02/Entities/UsedProduct.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Exercise02.Entities
 {
-    class UsedProduct
+    class UsedProduct : Product
     {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct() { }
+
+        public UsedProduct(string name, double price, DateTime manufactureDate) : base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string PriceTag()
+        {
+            return Name
+                + " (Used) $"
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + " (Manufacture Date: "
+                + ManufactureDate.ToString("dd/MM/yyyy")
+                + ")";
+        }
     }
 }
--- Secao10/Exercises/Exercise02/Exercise02/Program.cs
+++ Secao10/Exercises/Exercise02/Exercise02/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using Exercise02.Entities;
 
 namespace Exercise02
 {
@@ -10,6 +9,37 @@ namespace Exercise02
     {
         static void Main(string[] args)
         {
+            List<Product> product = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported(c / u / i) ?");
+                char option = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if(option == 'c')
+                {
+                    product.Add(new Product(name, price));
+                 
+                }else if(option == 'u')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    product.Add(new UsedProduct(name, price, date));
+                }
+                else
+                {
+                    Console.Write("Customs fee: ");
+                    double cus
+                }
+            }
+
         }
     }
 }
