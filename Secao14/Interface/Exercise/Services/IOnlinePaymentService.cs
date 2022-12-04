38d67670a51cd2d708152325793a4e2a6f7c8edb
André Lopes <75704128+andre-lopss@users.noreply.github.com>
2022-12-03 21:01:29 -0300

    InterfaceExercise

---
+++ Secao14/Interface/Exercise/Services/IOnlinePaymentService.cs
@@ -0,0 +1,9 @@
+﻿namespace Exercise.Services
+{
+    interface IOnlinePaymentService
+    {
+
+        double PaymentFee(double amount);
+        double Interest(double amount, int months);
+    }
+}
