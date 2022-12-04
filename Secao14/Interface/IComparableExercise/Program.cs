affc9d715716657b0ca9aaa74d98b1cbfb6aeffb
André Lopes <75704128+andre-lopss@users.noreply.github.com>
2022-12-04 14:21:54 -0300

    Exercicios usando o IComparable para ordenar lista

--- Secao14/Interface/IComparableExercise/Program.cs
+++ Secao14/Interface/IComparableExercise/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using IComparableExercise.Entities;
 
 namespace IComparableExercise
 {
@@ -10,19 +11,29 @@ namespace IComparableExercise
         {
             string path = @"C:\Users\andre\source\repos\curso-csharp\Secao14\Interface\in.txt";
 
+            //in.txt:
+                //Maria Brown,4300.00
+                //Alex Green,3100.00
+                //Bob Grey,3100.00
+                //Anna White,3500.00
+                //Alex Black,2450.00
+                //Eduardo Rose,4390.00
+                //Willian Red,2900.00
+                //Marta Blue,6100.00
+                //Alex Brown,5000.00
             try
             {
                 using(StreamReader sr = File.OpenText(path))
                 {
-                    List<string> list = new List<string>();
+                    List<Employee> list = new List<Employee>();
                     while (!sr.EndOfStream)
                     {
-                        list.Add(sr.ReadLine());
+                        list.Add(new Employee(sr.ReadLine()));
                     }
                     list.Sort();
-                    foreach(string str in list)
+                    foreach(Employee emp in list)
                     {
-                        Console.WriteLine(str);
+                        Console.WriteLine(emp);
                     }
                 }
             }catch(IOException e)
