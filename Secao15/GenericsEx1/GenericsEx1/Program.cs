0620f7a8546253ce32795ca14fc0cbbb4dd7975c
André Lopes <75704128+andre-lopss@users.noreply.github.com>
2022-12-04 18:37:18 -0300

    Solucao do exercicio adequada usando o Generics na implementacao

--- Secao15/GenericsEx1/GenericsEx1/Program.cs
+++ Secao15/GenericsEx1/GenericsEx1/Program.cs
@@ -10,7 +10,7 @@ namespace GenericsEx1
     {
         static void Main(string[] args)
         {
-            PrintService printService = new PrintService();
+            PrintService<int> printService = new PrintService<int>();
 
             Console.Write("How many values: ");
             int n = int.Parse(Console.ReadLine());
