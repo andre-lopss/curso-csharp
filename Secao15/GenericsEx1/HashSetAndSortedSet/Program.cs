52be2ad55862cb5a6aecf52b0de68f7ac278977a
André Lopes <andrelneres@hotmail.com>
2022-12-05 10:04:30 -0300

    Initial Commit HashSet

---
+++ Secao15/GenericsEx1/HashSetAndSortedSet/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSetAndSortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook"));
+
+            foreach(string obj in set)
+            {
+                Console.WriteLine(obj);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
