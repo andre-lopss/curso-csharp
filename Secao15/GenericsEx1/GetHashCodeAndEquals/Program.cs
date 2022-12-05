69628d76651511590fdf415069b3ec72c3406684
André Lopes <andrelneres@hotmail.com>
2022-12-05 09:24:19 -0300

    Aprendendo GetHashCode e  Equals

---
+++ Secao15/GenericsEx1/GetHashCodeAndEquals/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using GetHashCodeAndEquals.Entities;
+
+namespace GetHashCodeAndEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string a = "Maria";
+            //string b = "João";
+            //Console.WriteLine(a.Equals(b));
+            //Console.WriteLine(a.GetHashCode());
+            //Console.WriteLine(b.GetHashCode());
+            Client a = new Client() { Name = "André", Email = "andre@hotmail.com" };
+            Client b = new Client() { Name = "Maria", Email = "maria@hotmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+
+            Console.ReadLine();
+        }
+    }
+}
