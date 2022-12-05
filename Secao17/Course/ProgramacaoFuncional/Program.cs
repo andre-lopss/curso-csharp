0d17c52eecbb02148d6f4cc612f2c246e47e8bf9
André Lopes <75704128+andre-lopss@users.noreply.github.com>
2022-12-05 18:15:08 -0300

    Exemplo de função que não é referencialmente transparente

---
+++ Secao17/Course/ProgramacaoFuncional/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramacaoFuncional
+{
+    class Program
+    {
+
+        public static int globalValue = 3;
+
+        static void Main(string[] args)
+        {
+            int[] vect = new int[] { 3, 4, 5 };
+            ChangeOddValues(vect);
+            Console.WriteLine(string.Join(" ", vect));
+
+            Console.ReadLine();
+        }
+
+        public static void ChangeOddValues(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 != 0)
+                {
+                    numbers[i] += globalValue;
+                }
+            }
+        }
+    }
+}
+
