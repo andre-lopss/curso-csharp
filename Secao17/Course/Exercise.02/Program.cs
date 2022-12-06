803591e878f3729799c3e641fae6f8e04c087598
André Lopes <andrelneres@hotmail.com>
2022-12-06 15:56:24 -0300

    Inicio do Exercicio 02

---
+++ Secao17/Course/Exercise.02/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Exercise02.Entities
+
+namespace Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+        }
+    }
+}
---
+++ Secao17/Course/Exercise02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Exercise02.Entities;
+using System.IO;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Employee> emp = new List<Employee>();
+
+            using(StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    string name = fields[0];
+                    string email = fields[1];
+                    double price = double.Parse(fields[2], CultureInfo.InvariantCulture);
+
+                    emp.Add(new Employee(name, email, price));
+                }
+            }
+
+            Console.Write("Enter salary: ");
+            double salary = double.Parse(Console.ReadLine());
+        }
+    }
+}
