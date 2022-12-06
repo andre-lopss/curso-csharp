8b379a65e857bcaf152636d84d1dc06b6f8a6a54
André Lopes <andrelneres@hotmail.com>
2022-12-06 10:25:52 -0300

    Musticast delegates

--- Secao17/Course/Delegates/Program.cs
+++ Secao17/Course/Delegates/Program.cs
@@ -1,24 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Delegates.Services;
 
 namespace Delegates
 {
-    delegate double BinaryNumericOperation(double n1, double n2);
+    delegate void BinaryNumericOperation(double n1, double n2);
     class Program
     {
         static void Main(string[] args)
         {
             double a = 10;
             double b = 12;
-
-            BinaryNumericOperation op = CalculationServices.Sum;
-
-            double result = op(a, b);
-            Console.WriteLine(result);
+            BinaryNumericOperation op = CalculationServices.ShowSum;
+            op += CalculationServices.ShowMax;
+            op(a, b);
 
             Console.ReadLine();
         }
--- Secao17/Course/Delegates/Services/CalculationServices.cs
+++ Secao17/Course/Delegates/Services/CalculationServices.cs
@@ -1,24 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Delegates.Services
 {
     class CalculationServices
     {
-        public static double Max(double x, double y)
+        public static void ShowMax(double x, double y)
         {
-            return (x > y) ? x : y;
+            double max = (x > y) ? x : y;
+            Console.WriteLine(max);
         }
-        public static double Sum(double x, double y)
+        public static void ShowSum(double x, double y)
         {
-            return x + y;
-        }
-        public static double Square(double x)
-        {
-            return x * x;
+            double sum = x + y;
+            Console.WriteLine(sum);
         }
     }
 }
